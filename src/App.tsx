65a6524854896e6fa4cba9a82989e065f5c2e619
Daniel Ors <do7811@gmail.com>
2022-02-21 23:48:30 -0500

    FEDLE initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -245,6 +245,8 @@ function App() {
     }
   }
 
+  console.debug('solution:', solution)
+
   return (
     <div className="pt-2 pb-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8 mt-20">
